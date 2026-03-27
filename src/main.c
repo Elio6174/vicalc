07ed53b23d93dc13e791629e6ff7d026fa7195bd
Elio6174 <elseismylife@gmail.com>
2026-03-26 23:05:29 -0600

    primer cuadro

--- src/main.c
+++ src/main.c
@@ -1,61 +1,22 @@
-#include <deflateCompress.h>
 #include <stdio.h>
-#include <conio.h>
+#include <curses.h>
 
 void imprimirBuffer(const char *buffer);
 
 int main(int argc, char *argv[]){
-	
-	const char *BUFFER = "Este es un mensaje que espero que tenga mucha repeticion de letras en su interior";
+	initscr();
+	noecho();
+	curs_set(0);
 
-	imprimirBuffer(BUFFER);
-	deflateCompression(BUFFER);
+	int yMax, xMax;
+	getmaxyx(stdscr, yMax, xMax);
 
+	WINDOW *win = newwin(yMax / 2, xMax / 2, yMax / 4, xMax / 4);
+	box(win, 0, 0);
 
+	wgetch(win);
 
-
-	u32 arry[]= {5,2,1,6};
-
-	for(int i = 0; i < 4; i++){
-		printf("%d ", arry[i]);
-	}
-	printf("\n");
-
-	heap_sort(arry, 4);
-
-	
-	for(int i = 0; i < 4; i++){
-		printf("%d ", arry[i]);
-	}
-	printf("\n");
-
-
-
-	int a = 4;
-	printf("Variable a: %d || a << 1: %d\n", a, a<<1);
-	/*
-	if(argc > 1){
-		FILE *archivo;
-		printf("El parametro recividio es: %s\n", argv[1]);
-		archivo = fopen(argv[1], "rb");
-
-		if(archivo == NULL) 
-			printf("error al abrir el archivo\n");
-
-		int car;
-		while((car = fgetc(archivo)) != EOF){
-			printf("%c", car);
-		}
-
-		fclose(archivo);
-
-	}*/
-
-
-	char tecla;
-	tecla = _getch();
-	printf("la tecla: %d\n", tecla);
-
+	endwin();
 	return 0;
 }
 
